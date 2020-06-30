4f7e5b1754ecff32483942f06ceb9c1219e2975f
Samuel Tilly <samuel.tilly@gmail.com>
2020-06-30 23:49:14 +0200

    use data files instead of switches

---
+++ packages/parser/src/util/killMessageData/genderMessages.ts
@@ -0,0 +1,12 @@
+export default {
+  "MOD_KAMIKAZE": ["goes out with a bang"],
+  "MOD_GRENADE_SPLASH": ["tripped on his own grenade", "tripped on her own grenade", "tripped on its own grenade"],
+  "MOD_ROCKET_SPLASH": ["blew himself up", "blew herself up", "blew itself up"],
+  "MOD_PLASMA_SPLASH": ["melted himself", "melted herself", "melted itself"],
+  "MOD_BFG_SPLASH": ["should have used a smaller gun"],
+  "MOD_PROXIMITY_MINE": ["found his prox mine", "found her prox mine", "found its prox mine"],
+  "DEFAULT": ["killed himself", "killed herself", "killed itself"]
+} as {
+      DEFAULT: string[],
+      [key: string]: string[]
+  }
---
+++ packages/parser/src/util/killMessageData/moduleMessages.ts
@@ -0,0 +1,12 @@
+export default {
+  "MOD_SUICIDE": "suicides",
+  "MOD_FALLING": "cratered",
+  "MOD_CRUSH": "was squished",
+  "MOD_WATER": "sank like a rock",
+  "MOD_SLIME": "melted",
+  "MOD_LAVA": "does a back flip into the lava",
+  "MOD_TARGET_LASER": "saw the light",
+  "MOD_TRIGGER_HURT": "was in the wrong place"
+} as {
+    [key: string]: string,
+}
