45187bd0422156bea76d3214cdfa09716ae530c3
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2020-07-03 08:07:19 +0200

    Added more parser tests

--- packages/parser/src/killParser.spec.ts
+++ packages/parser/src/killParser.spec.ts
@@ -22,3 +22,39 @@ test('A proper kill event', () => {
     targetScore: 0
   })
 })
+
+test('Suicide should result in a negative score', () => {
+  expect(
+    killParser('Kill: 2 2 10 2: fittoter killed fittoter by MOD_ROCKET_SPLASH')
+  ).toStrictEqual({
+    name: Q3Event.KILL,
+    arenaIndex: '2',
+    attacker: 'fittoter',
+    attackerIndex: '2',
+    attackerScore: -1,
+    message: '^7fittoter ^7blew itself up',
+    q3module: Q3Module.ROCKET_SPLASH,
+    modIndex: '10',
+    target: 'fittoter',
+    targetIndex: '2',
+    targetScore: 0
+  })
+})
+
+test('Killed by world', () => {
+  expect(
+    killParser('Kill: 0 2 10 2: <world> killed fittoter by MOD_WATER')
+  ).toStrictEqual({
+    name: Q3Event.KILL,
+    arenaIndex: '2',
+    attacker: '<world>',
+    attackerIndex: '0',
+    attackerScore: 0,
+    message: '^7fittoter ^7sank like a rock',
+    q3module: Q3Module.WATER,
+    modIndex: '10',
+    target: 'fittoter',
+    targetIndex: '2',
+    targetScore: -1
+  })
+})
