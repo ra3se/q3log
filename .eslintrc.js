e68599f1206dcac6fa0c8983df3e9124d239fa76
Samuel Tilly <samuel.tilly@gmail.com>
2021-01-25 01:03:11 +0100

    Some linting updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,64 +1,64 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: [
-    'plugin:unicorn/recommended'
-  ],
+  extends: ["plugin:unicorn/recommended", "prettier"],
   rules: {
-    'no-void': ['error', { allowAsStatement: true }],
-    'max-depth': 'error',
-    'max-len': ['error', { code: 120 }],
-    'max-lines-per-function': 'error',
-    'max-lines': 'error',
-    'max-nested-callbacks': 'error',
-    'max-params': 'error',
-    'max-statements': 'error',
-    'unicorn/filename-case': ['error', { cases: { camelCase: true, pascalCase: true } }],
-    'unicorn/no-array-reduce': 'off',
-    'unicorn/prevent-abbreviations': 'off',
-    'unicorn/no-useless-undefined': 'off'
+    "no-void": ["error", { allowAsStatement: true }],
+    "max-depth": "error",
+    "max-len": ["error", { code: 120 }],
+    "max-lines-per-function": "error",
+    "max-lines": "error",
+    "max-nested-callbacks": "error",
+    "max-params": "error",
+    "max-statements": "error",
+    "unicorn/filename-case": [
+      "error",
+      { cases: { camelCase: true, pascalCase: true } },
+    ],
+    "unicorn/no-array-reduce": "off",
+    "unicorn/prevent-abbreviations": "off",
+    "unicorn/no-useless-undefined": "off",
   },
   overrides: [
     {
-      files: ['*.js', '*.jsx', '**/*/bin/*'],
-      extends: ['standard'],
+      files: ["*.js", "*.jsx", "**/*/bin/*"],
+      extends: ["standard", "prettier", "prettier/standard"],
       rules: {
-        '@typescript-eslint/no-var-requires': 'off',
-        'unicorn/filename-case': ['error', { case: 'kebabCase' }]
-      }
-    },
-    {
-      files: ['*.ts', '*.tsx'],
-      extends: ['standard-with-typescript'],
-      rules: {
-        '@typescript-eslint/no-unused-vars': 'error',
-        'node/no-unsupported-features/es-syntax': 'off',
-        'node/no-missing-import': 'off'
+        "unicorn/filename-case": ["error", { case: "kebabCase" }],
       },
-      parserOptions: {
-        project: './packages/**/tsconfig.json'
-      }
     },
     {
-      files: ['**/*spec*'],
-      plugins: ['jest'],
+      files: ["*.ts", "*.tsx"],
       extends: [
-        'plugin:jest/recommended',
-        'plugin:jest/style'
+        "standard-with-typescript",
+        "prettier",
+        "prettier/@typescript-eslint",
+        "prettier/standard",
       ],
       rules: {
-        'max-lines-per-function': 'off'
-      }
-    }
+        "@typescript-eslint/no-unused-vars": "error",
+        "node/no-unsupported-features/es-syntax": "off",
+        "node/no-missing-import": "off",
+      },
+      parserOptions: {
+        project: "./packages/**/tsconfig.json",
+      },
+    },
+    {
+      files: ["**/*spec*"],
+      plugins: ["jest"],
+      extends: ["plugin:jest/recommended", "plugin:jest/style"],
+      rules: {
+        "max-lines-per-function": "off",
+      },
+    },
   ],
-  ignorePatterns: [
-    '**/lib/**/*.js'
-  ],
+  ignorePatterns: ["**/lib/**/*.js"],
   settings: {
-    'import/parsers': {
-      'import/internal-regex': ['^@q3log/']
-    }
-  }
-}
+    "import/parsers": {
+      "import/internal-regex": ["^@q3log/"],
+    },
+  },
+};
--- packages/parser/src/util/generateKillMessage.ts
+++ packages/parser/src/util/generateKillMessage.ts
@@ -1,15 +1,15 @@
-import { Q3Module, Q3Gender, Q3World } from '@q3log/types'
-import { moduleMessages } from './killMessageData/moduleMessages'
-import { targetMessages } from './killMessageData/targetMessages'
-import { genderMessages } from './killMessageData/genderMessages'
+import { Q3Module, Q3Gender, Q3World } from "@q3log/types";
+import { moduleMessages } from "./killMessageData/moduleMessages";
+import { targetMessages } from "./killMessageData/targetMessages";
+import { genderMessages } from "./killMessageData/genderMessages";
 
 interface killMessage {
-  attacker: string
-  attackerIndex: string
-  gender: string
-  q3module: Q3Module
-  target: string
-  targetIndex: string
+  attacker: string;
+  attackerIndex: string;
+  gender: string;
+  q3module: Q3Module;
+  target: string;
+  targetIndex: string;
 }
 
 const genderMessage = (q3module: Q3Module, gender: string): string => {
@@ -20,21 +20,24 @@ const genderMessage = (q3module: Q3Module, gender: string): string => {
   ): string => {
     switch (gender) {
       case Q3Gender.MALE:
-        return maleMessage
+        return maleMessage;
       case Q3Gender.FEMALE:
-        return femaleMessage
+        return femaleMessage;
       default:
-        return neuterMessage
+        return neuterMessage;
     }
-  }
+  };
 
-  const m = genderMessages[q3module] ?? genderMessages.DEFAULT
-  return parseGenderMessage(m[0 % m.length], m[1 % m.length], m[2 % m.length])
-}
+  const m = genderMessages[q3module] ?? genderMessages.DEFAULT;
+  return parseGenderMessage(m[0 % m.length], m[1 % m.length], m[2 % m.length]);
+};
 
-const addMessageColors = (parts: string[]): string => parts.reduce(
-  (result, current, index) =>
-    result + `${index > 0 && index < 3 ? ' ' : ''}^7${current}`, '')
+const addMessageColors = (parts: string[]): string =>
+  parts.reduce(
+    (result, current, index) =>
+      result + `${index > 0 && index < 3 ? " " : ""}^7${current}`,
+    ""
+  );
 
 export default ({
   attacker,
@@ -42,25 +45,27 @@ export default ({
   gender,
   q3module,
   target,
-  targetIndex
+  targetIndex,
 }: killMessage): string => {
   const finilizeMessage = (part1?: string, part2?: string): string => {
-    if (part1 !== undefined) {
-      return addMessageColors(part2 !== undefined
-        ? [target, part1, attacker, part2]
-        : [target, part1, attacker])
-    } else {
-      return ''
-    }
-  }
+    return part1 !== undefined
+      ? addMessageColors(
+          part2 !== undefined
+            ? [target, part1, attacker, part2]
+            : [target, part1, attacker]
+        )
+      : "";
+  };
 
   if (attackerIndex === targetIndex) {
-    return addMessageColors([target, genderMessage(q3module, gender)])
+    return addMessageColors([target, genderMessage(q3module, gender)]);
   }
 
   if (attacker !== Q3World) {
-    return finilizeMessage(...targetMessages[q3module] ?? targetMessages.DEFAULT)
+    return finilizeMessage(
+      ...(targetMessages[q3module] ?? targetMessages.DEFAULT)
+    );
   }
 
-  return addMessageColors([target, moduleMessages[q3module]])
-}
+  return addMessageColors([target, moduleMessages[q3module]]);
+};
